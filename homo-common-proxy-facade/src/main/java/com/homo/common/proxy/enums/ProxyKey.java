bcbed7f043af15ccd7b3c463d6f76fa8c491ddaa
hzm <1054743949@qq.com>
2023-07-18 14:37:07 +0800

    1、移除common-proxy说明文档

--- homo-common-proxy-facade/src/main/java/com/homo/common/proxy/enums/ProxyKey.java
+++ homo-common-proxy-facade/src/main/java/com/homo/common/proxy/enums/ProxyKey.java
@@ -6,10 +6,10 @@ public interface ProxyKey {
     String MSG_KEY = "msg";
 
     String X_HOMO_APP_ID = "X-Homo-App-Id";
-    String X_HOMO_TOKEN = "X-Homo-token";
-    String X_HOMO_USER_ID = "X-Homo-user-Id";
-    String X_HOMO_CHANNEL_ID = "X-Homo-channel-Id";
-    String X_HOMO_SIGNATURE = "X-Homo-signature";
+    String X_HOMO_TOKEN = "X-Homo-Token";
+    String X_HOMO_USER_ID = "X-Homo-User-Id";
+    String X_HOMO_CHANNEL_ID = "X-Homo-Channel-Id";
+    String X_HOMO_SIGNATURE = "X-Homo-Signature";
     String X_HOMO_RESPONSE = "X-Homo-Response";
     String X_HOMO_RESPONSE_TIME = "X-Homo-Response-Time";
 }
